f0a4885406e4f83068b33b5fcb4829c53271a49f
GJ <gjmcferrin@gmail.com>
2013-03-20 01:14:02 -0400

    Add conflictsWith method to ItemMeta. Adds BUKKIT-3830.

--- src/main/java/org/bukkit/inventory/meta/ItemMeta.java
+++ src/main/java/org/bukkit/inventory/meta/ItemMeta.java
@@ -107,6 +107,14 @@ public interface ItemMeta extends Cloneable, ConfigurationSerializable {
      */
     boolean removeEnchant(Enchantment ench);
 
+   /**
+    * Checks if the specified enchantment conflicts with any enchantments in this ItemMeta.
+    *
+    * @param ench Enchantment to test
+    * @return true if the enchantment conflicts, false otherwise
+    */
+    boolean conflictsWith(Enchantment ench);
+
     @SuppressWarnings("javadoc")
     ItemMeta clone();
 }
