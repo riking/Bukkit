885bdb92495cb6227a241a6c89385fa538149449
riking <rikingcoding@gmail.com>
2013-03-24 11:47:28 -0700

    Use BiteChance implementation

--- src/main/java/org/bukkit/entity/Fish.java
+++ src/main/java/org/bukkit/entity/Fish.java
@@ -1,5 +1,7 @@
 package org.bukkit.entity;
 
+import org.bukkit.Bukkit;
+
 /**
  * Represents a fishing hook.
  */
@@ -7,22 +9,83 @@ public interface Fish extends Projectile {
 
     /**
      * Gets the chance of a fish biting.
-     *
-     * 0.0 = No Chance.
-     * 1.0 = Instant catch.
-     *
-     * @return chance
+     * @return a BiteChance object
      */
-    public double getBiteChance();
+    public BiteChance getBiteChance();
 
     /**
      * Sets the chance of a fish biting.
-     *
-     * 0.0 = No Chance.
-     * 1.0 = Instant catch.
-     *
-     * @param chance
+     * @param a BiteChance object
      */
-    public void setBiteChance(double chance);
+    public void setBiteChance(BiteChance chance);
 
+    /**
+     * Construct a new BiteChance for this FishHook.
+     * @param clear chance per tick of a bite in clear weather
+     * @param rainy chance per tick of a bite in rainy weather
+     * @return a BiteChance with the given values
+     */
+    public BiteChance constructBiteChance(double clear, double rainy);
+
+    /**
+     * A class to represent the chance of a fish biting. Chances are evaluated
+     * every 1/20th of a second.
+     * <p />
+     * A BiteChance belongs to a fishing hook and should not be reused,
+     * although there are checks in place to allow this.
+     */
+    public abstract class BiteChance {
+
+        /**
+         * The vanilla chance per tick of a fish biting in clear weather.
+         */
+        public static final double DEFAULT_CLEAR_CHANCE = 1 / 500d;
+
+        /**
+         * The vanilla chance per tick of a fish biting in rainy weather.
+         */
+        public static final double DEFAULT_RAINY_CHANCE = 1 / 300d;
+
+        public double clear_chance = DEFAULT_CLEAR_CHANCE;
+        public double rainy_chance = DEFAULT_RAINY_CHANCE;
+
+        /**
+         * Gets the current chance of a fish biting, based on the current
+         * weather the fishing hook is in.
+         * @return chance per tick of a bite
+         */
+        public abstract double getChance();
+
+        /**
+         * Get the chance given by this BiteChance in clear weather.
+         * @return chance per tick of a bite given clear weather
+         */
+        public double getClearBiteChance() {
+            return clear_chance;
+        }
+
+        /**
+         * Get the chance given by this BiteChance in rainy weather.
+         * @return chance per tick of a bite given rainy weather
+         */
+        public double getRainyBiteChance() {
+            return rainy_chance;
+        }
+
+        /**
+         * Set the chance given by this BiteChance in clear weather.
+         * @param chance per tick of a bite given clear weather
+         */
+        public void setClearBiteChance(double chance) {
+            clear_chance = chance;
+        }
+
+        /**
+         * Set the chance given by this BiteChance in rainy weather.
+         * @param chance per tick of a bite given rainy weather
+         */
+        public void setRainyBiteChance(double chance) {
+            rainy_chance = chance;
+        }
+    }
 }
